d0c6e0668cadf1b0277c88968652f5291d96831f
Gerardo P.C <fpd4861@edu.xunta.gal>
2024-05-02 23:19:53 +0200

    validación inicial

--- Tarefa 07/js/app.js
+++ Tarefa 07/js/app.js
@@ -1,3 +1,20 @@
 // Código de validaciones del formulario
+let expresionNombre = /^[a-zA-ZñÑ\s]{1,20}$/;
+let expresionApellidos = /^[a-zA-ZñÑ\s]{1,40}$/;
+
+function validarNombre(){
+    let nombre=document.querySelector("#nombre");
+    return nombre.value.match(expresionNombre)?true:false;
+}
+function validarApellidos(){
+    let apellidos=document.querySelector("#apellidos");
+    return apellidos.value.match(expresionApellidos)?true:false;
+}
 
 // Envio de datos con XMLHttpRequest
+let formulario = document.forms[0];
+formulario.onsubmit = ()=>{
+    console.log(validarNombre());
+    console.log(validarApellidos());
+
+    return false;}
